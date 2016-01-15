ff1df8619111e706a2b234917da5aa10efb68252
Mustafa Tasdemir <mustafa.tasdemir@hotmail.com.tr>
2016-01-15 20:10:03 +0200

    new main menu layout has been added. needs a lot of improvement

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,9 +4,14 @@
 
 <title>Super Cool Layouts</title>
 
-<link rel="stylesheet" href="{{ URL::asset('css/dropdown.css') }}" />
+<!--<link rel="stylesheet" href="{{ URL::asset('css/dropdown2.css') }}" />-->
+<link rel="stylesheet" href="{{ URL::asset('css/dropotron.main.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}" />
 <!-- load bootstrap from a cdn -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"> -->
 
 <script type="text/javascript" src="{{ URL::asset('js/jquery-2.2.0.min.js') }}"></script>
-<script type="text/javascript" src="{{ URL::asset('js/jquery.dropdown.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/jquery.dropotron.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/jquery.dropotron.skel.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/jquery.dropotron.util.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/jquery.dropotron.main.js') }}"></script>
